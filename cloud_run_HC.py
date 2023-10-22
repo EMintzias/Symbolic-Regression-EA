aa6d48785dce52148b4a480d3a597f0157996911
polbebe <p.bernat@columbia.edu>
2023-10-22 02:54:45 -0400

    another

--- cloud_run_HC.py
+++ cloud_run_HC.py
@@ -624,7 +624,7 @@ if __name__ == '__main__':
     # PARALLEL HC LEARNING CURVE PLOT
     files = ['Silver.txt', 'Gold.txt', 'Platinum.txt']
     for i in tqdm(range(len(files)), desc='Files:', leave=False):
-        level = i #input("Enter level (Bronze.txt, Silver.txt, Gold.txt): ")
+        level = files[i] #input("Enter level (Bronze.txt, Silver.txt, Gold.txt): ")
         data = np.loadtxt(level, dtype=float, delimiter=',')
         Y_range = (np.min(data[:, 1]), np.max(data[:, 1]))
         iterations = 5
