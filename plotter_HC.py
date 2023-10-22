46289b1a154b91509f6fe9885b23e4ba44a8bd2a
polbebe <p.bernat@columbia.edu>
2023-10-22 12:08:12 -0400

    creating subplots for best solutions

--- plotter_HC.py
+++ plotter_HC.py
@@ -570,140 +570,41 @@ plt.savefig('{}/HC_Learning_Curve_{}evals.pdf'.format(folder, x_largest), dpi=30
 plt.show()
 
 
-
-#%%
-# LOAD DATA
-level = 'Bronze.txt'
-folder = 'Results_{}'.format(level)
-filename = '{}/RS_date_Oct-21_22-21_5_tests_10000_evals.pkl'.format(folder)
-# Open the file in read-binary mode ('rb') to read the data.
-with open(filename, 'rb') as file:
-    # Use pickle.load() to load the data from the file.
-    data = pickle.load(file)
-
-
-
-#%%
-# PREPARE DATA
-
-x_arr = np.full((len(data),len(data[0][1])), None, dtype=float)
-y_arr = np.full((len(data),len(data[0][1])), None, dtype=float)
-
-for i in range(len(data)):
-    for j in range(len(data[0][1])):
-        x_arr[i][j] = j
-        y_arr[i][j] = data[i][1][j][1]
-
-x_mean = np.mean(np.array(x_arr), axis=0)
-y_mean = np.mean(np.array(y_arr),axis=0)
-
-errors = np.std(np.array(y_arr), axis=0) / np.sqrt(np.array(y_arr).shape[0])
-x_err = []
-y_err = []
-err = []
-for i in range(len(data[0][1])):
-    if i % 2499 == 0 and i != 0:
-        print(i)
-        x_err.append(i)
-        y_err.append(y_mean[i])
-        err.append(errors[i])
-print(len(err))
-
-# %%
-# PLOT LEARNING CURVE
-plt.figure(figsize=(10, 10))
-plt.plot(x_mean, y_mean, '-', label='RS', color='#3CB371')
-plt.errorbar(x_err, y_err, yerr=err, color='#3CB371', fmt='o', capsize=5, markersize=4)
-plt.title("Random Search Learning Curve for '{}' (tests: {})".format(folder, len(data)))
-plt.xlabel('Evaluations')
-plt.ylabel('MSE')
-plt.yscale('log')
-plt.legend()
-plt.grid(True)
-plt.savefig('{}/RS_Learning_Curve_{}evals.pdf'.format(folder, len(x_mean)), dpi=300)
-plt.show()
-
 # %%
 # PLOT AGAINST FUNCTION
-true_data = np.loadtxt('Silver.txt', dtype=float, delimiter=',')
+true_data = np.loadtxt(level, dtype=float, delimiter=',')
 
+for j in range(len(data)):
+    x_plot = np.full(len(true_data.T[0]), None, dtype=float)
+    y_plot = np.full(len(true_data.T[0]), None, dtype=float)
+
+    tree = data[j][0]
+    MSE = round(data[j][0].MSE, 8)
+    figure_text = f"Heap:\n\n{tree}\n\n\nMSE:\n\n{MSE}"
+
+    for i, x in enumerate(true_data.T[0]):
+        x_plot[i] = x
+        y_plot[i] = data[j][0].evaluate(X=x)
+
+    fig, ax = plt.subplots(figsize=(13.7, 10))
+    fig.subplots_adjust(right=0.7)
+    # PLOT Best Solution vs True Data
+    ax.plot(true_data.T[0], true_data.T[1], '-', label=level, color='black')
+    ax.plot(x_plot, y_plot, '-', label='HC Solution', color='darkorange')
+    ax.set_title("Parallel Hill Climber Solution for '{}' (test #{})".format(level, j))
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.legend()
+    ax.grid(True)
+    ax.text(1.05, 0.5, figure_text, transform=ax.transAxes, ha='left', va='center')
+    folder_path = '{}/Subplots'.format(folder)
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+    fig.savefig('{}/HC_Solution_test#{}.pdf'.format(folder_path, j), dpi=300)
+    plt.show()
+
+#%%
 for i in range(len(data)):
     data[i][0].plot_approximation(target_data=true_data)
     print(data[i][0])
     print(data[i][0].MSE)
-
-# %%
-
-def HC(target_data, step_search_size=128, max_depth=3, mutate_prcnt_change=.01,
-       const_prob=.5, C_range=(-10, 10), given_function=None, Optimized_random=0):
-    '''
-    This function will search random children and move in the best direction from an optimized random start. 
-
-    '''
-    if not given_function:
-        # initialize return functions
-        if Optimized_random:  # does a quick random search to eliminate the trash
-            # TODO diversity issue this should be deliberately implemented at the random function level
-            Best_Function, _ = Random_Search(evaluations=Optimized_random,
-                                             data=target_data,
-                                             max_depth=max_depth,
-                                             C_range=C_range)
-        else:
-            Best_Function = NP_Heap(length=32)
-            Best_Function.Random_Heap(max_depth=max_depth,
-                                      const_prob=const_prob,
-                                      C_range=C_range)
-    else:
-        Best_Function = given_function
-
-    Min_MSE = Best_Function.get_MSE(target_data)
-    MSE_log = []
-    Improved = True
-    step_num = 0
-    while Improved:
-        print(step_num)
-        Improved = False  # to be flagged true if any of the children is better than the parent
-        # parent of all children to search steps based on the curent best
-        gen_parent = Best_Function.copy()
-        for _ in range(step_search_size):
-            # loops N times testing nearby points
-            step = gen_parent.copy()
-            step.Constant_Mutation(change_prcnt=mutate_prcnt_change)
-            step_MSE = step.get_MSE(target_data)
-            if step_MSE < Min_MSE:
-                # this will track best step at a position.
-                Best_Function = step
-                Min_MSE = step_MSE
-                Improved = True  # only needs to happen once to be overritedn
-        step_num += 1
-        MSE_log.append([step_num*step_search_size, Min_MSE])
-
-        #print('loop ', step_num, ' DONE')
-        #print('Best child \n',Best_Function)
-        #print('min MSE', Min_MSE)
-
-    return Best_Function, MSE_log
-
-
-# %%
-
-step_search_size=128
-max_depth=3
-mutate_prcnt_change=.02
-const_prob=.5
-C_range=(-10, 10)
-Optimized_random=0
-
-function, mse_arr = HC(step_search_size=step_search_size,
-                               target_data=true_data,
-                               mutate_prcnt_change=mutate_prcnt_change,
-                               max_depth=max_depth,
-                               const_prob=const_prob,
-                               C_range=C_range,
-                               given_function=data[2][0],
-                               Optimized_random=Optimized_random)
-
-# %%
-function.plot_approximation(target_data=true_data)
-print(function.MSE)
-# %%
